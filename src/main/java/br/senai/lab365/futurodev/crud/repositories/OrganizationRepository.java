0985880eb548a392a982141db80b16071b8bb6a6
Louise Morais <louisemorais3@gmail.com>
2025-04-14 19:48:56 -0300

    feat: adicionando método JPQL para pesquisas por nome nos repositories de Projects e Organizations

--- src/main/java/br/senai/lab365/futurodev/crud/repositories/OrganizationRepository.java
+++ src/main/java/br/senai/lab365/futurodev/crud/repositories/OrganizationRepository.java
@@ -2,8 +2,12 @@ package br.senai.lab365.futurodev.crud.repositories;
 
 import br.senai.lab365.futurodev.crud.models.Organization;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface OrganizationRepository extends JpaRepository<Organization, Long> {
+    List<Organization> findByNameContainingIgnoreCase(String name);
 }
--- src/main/java/br/senai/lab365/futurodev/crud/repositories/ProjectRepository.java
+++ src/main/java/br/senai/lab365/futurodev/crud/repositories/ProjectRepository.java
@@ -1,7 +1,11 @@
 package br.senai.lab365.futurodev.crud.repositories;
 
+import br.senai.lab365.futurodev.crud.models.Organization;
 import br.senai.lab365.futurodev.crud.models.Project;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ProjectRepository extends JpaRepository<Project, Long> {
+    List<Project> findByRegionContainingIgnoreCase(String name);
 }
